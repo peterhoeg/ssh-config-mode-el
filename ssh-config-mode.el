b575b42cd7121a937f1352e24bcdc9c847607004
Ville Skyttä <ville.skytta@iki.fi>
2021-07-10 14:24:09 +0300

    auto-mode-alist: fix `knownhosts` typo in commentary

--- ssh-config-mode.el
+++ ssh-config-mode.el
@@ -20,7 +20,7 @@
 ;;   (autoload 'ssh-config-mode "ssh-config-mode" t)
 ;;   (add-to-list 'auto-mode-alist '("/\\.ssh/config\\(\\.d/.*\\.conf\\)?\\'" . ssh-config-mode))
 ;;   (add-to-list 'auto-mode-alist '("/sshd?_config\\(\\.d/.*\\.conf\\)?\\'"  . ssh-config-mode))
-;;   (add-to-list 'auto-mode-alist '("/knownhosts\\'"       . ssh-known-hosts-mode))
+;;   (add-to-list 'auto-mode-alist '("/known_hosts\\'"       . ssh-known-hosts-mode))
 ;;   (add-to-list 'auto-mode-alist '("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
 ;;   (add-hook 'ssh-config-mode-hook 'turn-on-font-lock)
 
